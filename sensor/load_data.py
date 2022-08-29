06beb5a860ab8978d79fe47e41e66476ab2c0d34
Ruud Wolters <Hobb$t08>
2022-08-29 11:08:20 +0200

    first module

--- sensor/load_data.py
+++ sensor/load_data.py
@@ -6,7 +6,7 @@ import csv
 def load_sensor_data():
     """Load the sensor dta from csv."""
     sensor_data = []
-    sensor_files = glob.glob(os.path.join(os.getcwd(), '..\\datasets', '*.csv'))
+    sensor_files = glob.glob(os.path.join(os.getcwd(), 'datasets', '*.csv'))
     for sensor_file in sensor_files:
         with open(sensor_file) as data_file:
             data_reader = csv.DictReader(data_file, delimiter=',')
